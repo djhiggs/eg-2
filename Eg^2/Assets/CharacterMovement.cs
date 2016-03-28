c1fdecf664e6e4f3dbee8925e7025fa4e7bc1424
Rambo36988 <mysticrambo@gmail.com>
2016-03-28 18:14:26 -0500

    Initial Upload
    The first commit to the repo.

---
+++ Eg^2/Assets/CharacterMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+	GameObject mainCam;
+	private float zval;
+	private float xval;
+	private float jval;
+	public float camDistance;
+ 	Vector3 home;
+	public float camHeight;
+	Vector3 mainCamMove;
+	public float speed;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = GameObject.Find ("mainCamera");
+		home = new Vector3 (camDistance, camHeight, 0);
+		mainCamMove = new Vector3 ();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		zval = Input.GetAxis ("zHorizontal");
+		xval = Input.GetAxis ("xHorizontal");
+		mainCamMove.Set (camDistance, camHeight, -20 * zval); 
+
+		if (zval > 0) {
+			transform.Translate (0, 0, speed*zval);
+			mainCam.transform.localPosition = mainCamMove;
+
+			//rb.velocity = new Vector3(7, 1, 0);
+		}
+
+		if (zval < 0) {
+			transform.Translate (0, 0, speed*zval);
+			mainCam.transform.localPosition = mainCamMove;
+
+			//rb.velocity = new Vector3(-7, 1, 0);
+		}
+
+		if (zval == 0 && mainCam.transform.localPosition.z != 0) {
+			mainCam.transform.localPosition = home;
+		}
+
+		if (xval > 0) {
+			transform.Translate (speed*xval, 0, 0);
+			//rb.velocity = new Vector3(0, 1, 7);
+		}
+
+		if (xval < 0) {
+			transform.Translate (speed*xval, 0, 0);
+			//rb.velocity = new Vector3(0, 1, -7);
+		}
+			
+			
+		if (Input.GetButtonDown("Jump")) {
+			
+			rb.velocity = new Vector3(0, 5, 0);
+		}
+
+		if (Input.GetButtonDown("Fire1")) {
+
+			transform.rotation = Quaternion.Euler(0 , -90 , 0);
+		}
+	}
+}
---
+++ Eg^2/Assets/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pausePanel;
+    public bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+        isPaused = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPaused)
+        {
+            PauseGame(true);
+        }
+        else
+        {
+            PauseGame(false);
+        }
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SwitchPause();
+        }
+	
+	}
+    void PauseGame(bool state)
+    {
+        if (state)
+        {
+            Time.timeScale = 0.0f; //Paused
+        }
+        else
+        {
+            Time.timeScale = 1.0f; //Unpaused
+        }
+        pausePanel.SetActive(state);
+    }
+    public void SwitchPause()
+    {
+        if (isPaused)
+        {
+            isPaused = false; //Change the value
+        }
+        else
+        {
+            isPaused = true;
+        }
+    }
+}
