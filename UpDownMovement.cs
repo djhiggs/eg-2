6113c8eab1660816edc4f567b1580bf7ef0b715b
dannypoole <d0520588@amarillocollege.com>
2016-04-14 23:49:38 -0500

    Added files via upload

---
+++ UpDownMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UpDownMovement : MonoBehaviour {
+
+    public enum FollowType
+    {
+        MoveToward, Lerp
+    }
+
+    public FollowType Type = FollowType.MoveToward;
+    public PathDefinition Path;
+    public float Speed = 1;
+    public float MaxDistanceToGoal = .1f;
+
+    private IEnumerator<Transform> currentPoint;
+
+    public void Start()
+    {
+        currentPoint = Path.GetPathEnumerator();
+        currentPoint.MoveNext();
+
+        transform.position = currentPoint.Current.position;
+    }
+
+    public void Update()
+    {
+        if (Type == FollowType.MoveToward)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, currentPoint.Current.position, Time.deltaTime * Speed);
+        }
+        else if (Type == FollowType.Lerp)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, currentPoint.Current.position, Time.deltaTime * Speed);
+        }
+
+        var distanceSquared = (transform.position - currentPoint.Current.position).sqrMagnitude;
+        if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal)
+        {
+            currentPoint.MoveNext();
+        }
+    }
+}
