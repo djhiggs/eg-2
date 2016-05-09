656a3038960479698f063924c1efe1a43b99397b
SJReno <spencerreno@gmail.com>
2016-05-09 13:33:09 -0500

    Main Menu Scene
    need option menu code

---
+++ Main Menu/Assets/Scripts/MainMenuscript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuscript : MonoBehaviour {
+
+
+    public Canvas optionMenu;
+    public Button optionButton;
+    public Button optionBackground;
+    public Button startButton;
+    public Button startBackground;
+    public Button exitButton;
+    public Button exitBackground;
+    public Button loadButton;
+    public Button loadBackground;
+    public Canvas loadMenu;
+    
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        optionMenu = optionMenu.GetComponent<Canvas>();
+        optionButton = optionButton.GetComponent<Button>();
+        optionBackground = optionBackground.GetComponent<Button>();
+        startButton = startButton.GetComponent<Button>();
+        startBackground = startBackground.GetComponent<Button>();
+        exitButton = exitButton.GetComponent<Button>();
+        exitBackground = exitBackground.GetComponent<Button>();
+        loadButton = loadButton.GetComponent<Button>();
+        loadBackground = loadBackground.GetComponent<Button>();
+        loadMenu = loadMenu.GetComponent<Canvas>();
+       
+        optionMenu.enabled = false;
+        loadMenu.enabled = false;
+
+	}
+
+    public void OptionPress()
+    {
+        optionMenu.enabled = true;
+        startButton.enabled = false;
+        startBackground.enabled = false;
+        exitButton.enabled = false;
+        exitBackground.enabled = false;
+        loadButton.enabled = false;
+        loadBackground.enabled = false;
+        loadMenu.enabled = false;
+        optionButton.enabled = false;
+        optionBackground.enabled = false;
+
+
+        
+    }
+    public void LoadPress()
+    {
+        optionMenu.enabled = false;
+        startButton.enabled = false;
+        startBackground.enabled = false; 
+        exitButton.enabled = false;
+        exitBackground.enabled = false;
+        loadButton.enabled = false;
+        loadBackground.enabled = false;
+        loadMenu.enabled = true;
+        optionButton.enabled = false;
+        optionBackground.enabled = false;
+
+    }
+
+    public void NoPress()
+    {
+        optionMenu.enabled = false;
+        startButton.enabled = true;
+        startBackground.enabled = true;
+        exitButton.enabled = true;
+        exitBackground.enabled = true;
+        loadButton.enabled = true;
+        loadBackground.enabled = true;
+        loadMenu.enabled = false;
+        optionButton.enabled = true;
+        optionBackground.enabled = true;
+
+    }
+
+    //public void StartLevel()
+    //{
+    //    Application.LoadLevel(1);
+    //}
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+	
+	
+}
---
+++ Main Menu/Assets/Scripts/SettingManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SettingManager : MonoBehaviour {
+
+    public Toggle fullscreenToggle;
+    public Dropdown resolutionDropdown;
+    public Dropdown textureQualityDropdown;
+    public Dropdown antialiasingDropdown;
+    public Dropdown vSyncDropdown;
+    public Slider masterVolumeSlider;
+
+
+    public Resolution[] resolutions;
+    public GameSettings gameSettings;
+
+    //Check for mess up here
+
+    void OnEnable()
+    {
+        gameSettings = new GameSettings();
+
+        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullScreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        antialiasingDropdown.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
+        vSyncDropdown.onValueChanged.AddListener(delegate { OnVsyncChange(); });
+        masterVolumeSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChange(); });
+
+
+        resolutions = Screen.resolutions;
+    }
+    public void OnFullScreenToggle()
+    {
+       gameSettings.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+
+    }
+
+    public void OnResolutionChange()
+    {
+        Resolution[] resolutions = Screen.resolutions;
+        foreach (Resolution res in resolutions)
+        {
+            print(res.width + "x" + res.height);
+        }
+    }
+    public void OnTextureQualityChange()
+    {
+       QualitySettings.masterTextureLimit = gameSettings.textureQuality = textureQualityDropdown.value;
+        
+    }
+    public void OnAntialiasingChange()
+    {
+
+    }
+    public void OnVsyncChange()
+    {
+
+    }
+    public void OnMasterVolumeChange()
+    {
+
+    }
+    public void SaveSettings()
+    {
+
+    }
+    public void LoadSettings()
+    {
+
+    }
+}
